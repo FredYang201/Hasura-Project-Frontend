201b9723f627d3f2f1418fc09caf7eba4e8d55e3
FredYang201 <fredyang1402143595@gmail.com>
2020-10-14 12:42:05 -0700

    hasura project frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/Home'
+import NewEmployee from './pages/NewEmployee'
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return <Router>
+      <Switch>
+        <Route path='/create' >
+          <NewEmployee />
+        </Route>
+        <Route path='/home' >
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  }
 }
 
 export default App;
